ee833dfaf55133986791e113fbec84f8e7edc693
Mishunkhan <89736636+Mishunkhan@users.noreply.github.com>
2023-09-11 13:21:14 +0200

    categories now shown on category page
    fikset category fanne

--- MyShop/Controllers/CategoryController.cs
+++ MyShop/Controllers/CategoryController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Forum.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Forum.Controllers
 {
 	public class CategoryController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    {
+        public IActionResult Table()
+        {
+            var items = new List<Category>();
+            var item1 = new Category();
+
+            item1.Name = "Sport";
+
+
+            var item2 = new Category
+            {
+
+                Name = "Mat",
+
+            };
+
+            items.Add(item1);
+            items.Add(item2);
+
+            ViewBag.CurrentViewName = "List of Category";
+            return View(items);
+        }
+    }
 }
