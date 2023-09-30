6bbcb8ded16d1be3f86c3f54bc91854a3ccb5134
Miski <miskiyusuf@hotmail.com>
2023-09-30 19:08:07 +0200

    Koblingen mellom room og topic mangler

--- MyShop/Controllers/RoomController.cs
+++ MyShop/Controllers/RoomController.cs
@@ -24,7 +24,26 @@ namespace Forum.Controllers
             var rooms = await _roomRepository.GetAll();
             var roomListViewModel = new RoomListViewModel(rooms, "Table");
             return View(roomListViewModel);
+        }  
+        
+        //detalje for Room 
+        [HttpGet]
+        public async Task<IActionResult> RoomDetails(int roomId)
+        {
+            // Hent rommet fra databasen basert på rom-IDen
+            var room = await _roomRepository.GetItemById(roomId);
+
+            if (room == null)
+            {
+                return NotFound(); // Returner 404 hvis rommet ikke finnes
+            }
+
+            // Send rommet til visningen for romdetaljer
+            return View(room);
         }
+
+
+
         [HttpGet]
         public IActionResult CreateRoom(int categoryId)  //la til at man kan lage forum 
 
@@ -46,6 +65,77 @@ namespace Forum.Controllers
             }
             return View(room);
         }
+
+        // GET: Room/
+        [HttpGet]
+        public async Task<IActionResult> UpdateRoom(int roomId)
+        {
+            var room = await _roomRepository.GetItemById(roomId);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST: Room
+        public async Task<IActionResult> UpdateRoom(int roomId, Room room)
+        {
+            if (roomId != room.RoomId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _roomRepository.Update(room);
+                }
+                catch
+                {
+                }
+
+                return RedirectToAction(nameof(RoomTable));
+            }
+
+            return View(room);
+        }
+
+        // GET
+        [HttpGet]
+        public async Task<IActionResult> DeleteRoom(int roomId)
+        {
+            var room = await _roomRepository.GetItemById(roomId);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmedRoom(int roomId)
+        {
+            try
+            {
+                await _roomRepository.Delete(roomId);
+                return RedirectToAction(nameof(RoomTable));
+            }
+            catch
+            {
+                // Handle exceptions, if any
+                return RedirectToAction(nameof(RoomTable)); //Blir sendt tilbake til room table hvis noe skjer feil 
+            }
+        }
+
+
+
 
     }
 }
