fb679943912962915249ff52238b931329931759
Miski <miskiyusuf@hotmail.com>
2023-10-18 19:25:07 +0200

    Nå funker search som den skal

---
+++ MyShop/Controllers/SearchController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Forum.Models;
+
+namespace Forum.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly CategoryDbContext _context;
+
+        public SearchController(CategoryDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Search(string search)
+        {
+            if (string.IsNullOrWhiteSpace(search))
+            {
+                // Hvis søket er tomt, returner en tom liste
+                return View(new SearchResultViewModel());
+            }
+
+            var categories = _context.Categories.ToList()
+                .Where(category => category.Name.Contains(search, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+            var room = _context.Rooms.ToList()
+               .Where(room => room.RoomName.Contains(search, StringComparison.OrdinalIgnoreCase))
+               .ToList();
+
+            var posts = _context.Posts.ToList()
+                .Where(post => post.PostTitle.Contains(search, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            var topic = _context.Topics.ToList()
+                .Where(topic => topic.TopicName.Contains(search,StringComparison.OrdinalIgnoreCase)).ToList();
+
+            var searchResults = new SearchResultViewModel
+            {
+                Categories = categories,
+                Posts = posts,
+                Topics= topic,
+                Rooms = room
+            };
+
+            return View(searchResults);
+        }
+
+    }
+}
---
+++ MyShop/ViewModels/SearchResultViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Forum.Models;
+
+public class SearchResultViewModel
+{
+    public List<Category> Categories { get; set; }
+    public List<Post> Posts { get; set; }
+
+    public List<Topic> Topics { get; set; }
+    public List<Room> Rooms { get; internal set; }
+}
